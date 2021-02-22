116e7a16883963158befda4e323f29e91ee8ea49
yuzoogusu <syouta2970@gmail.com>
2021-02-22 09:27:29 +0900

    first commit

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField("タイトル", max_length=200)
+    content = models.TextField("本文")
+    created = models.DateTimeField("作成日", default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+# Create your models here.
